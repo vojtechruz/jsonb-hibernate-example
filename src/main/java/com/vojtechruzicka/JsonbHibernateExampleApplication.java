8c2b8c79563a8c7862ac4c1040cbde64893abfe6
Vojtech Ruzicka <vojtech.ruzicka@merck.com>
2017-03-12 15:42:33 +0100

    Initial project structure from Spring Initializr

---
+++ src/main/java/com/vojtechruzicka/JsonbHibernateExampleApplication.java
@@ -0,0 +1,12 @@
+package com.vojtechruzicka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonbHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonbHibernateExampleApplication.class, args);
+	}
+}
